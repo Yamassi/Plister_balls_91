3f199f0b5978daad73d7ef86d8a6a4d78945ef30
Yamassi <karateka_marateka@mail.ru>
2023-12-12 17:57:57 +0600

    Add_IAP_IDs

--- Assets/_Game/Scripts/Data/Const.cs
+++ Assets/_Game/Scripts/Data/Const.cs
@@ -6,7 +6,7 @@ public static class Const
 {
     public const int FirstCoins = 500, WaitGiftHours = 24;
     public const float FirstSoundVol = 1, FirstMusicVol = 1f;
-    public const string IAP_1 = "0";
-    public const string IAP_2 = "1";
-    public const string IAP_3 = "2";
+    public const string IAP_1 = "gold_5";
+    public const string IAP_2 = "gold_12";
+    public const string IAP_3 = "gold_20";
 }
